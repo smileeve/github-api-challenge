5c32b4f556f583e6daaaf1bfac8b574f0d0e8f56
Evelyn S <Sbluj@Evelyns-MBP.lan>
2018-12-24 05:01:17 -0600

    Func(my-repos):validate the Github API service consumption

--- src/app/modules/home/components/banner/banner.component.ts
+++ src/app/modules/home/components/banner/banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {GithubService} from 'src/app/core/services/github.service'
 
 @Component({
@@ -6,34 +6,34 @@ import {GithubService} from 'src/app/core/services/github.service'
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent implements OnInit {
+export class BannerComponent implements OnInit  {
 
   user:any;
   repositories:any;
   
   constructor(private githubservice: GithubService) {
 
-    this.user=  { "login":"smileeve", "company":"Company1","created_at":"2018-12-20T05:49:34Z","avatar_url":"https://avatars3.githubusercontent.com/u/46016048?v=4","html_url":"https://github.com/smileeve"};
+    // Dummy data for testing
+    /*this.user=  { "login":"smileeve", "company":"Company1","created_at":"2018-12-20T05:49:34Z","avatar_url":"https://avatars3.githubusercontent.com/u/46016048?v=4","html_url":"https://github.com/smileeve"};
     this.repositories=[
       {"name":"repo1","description":"kajflskjflskd","forks":"2","watchers":"20","has_issues":true},
       {"name":"repo2","description":"kajflskjflskd","forks":"4","watchers":"14","has_issues":false},
       {"name":"repo3","description":"kajflskjflskd","forks":"9","watchers":"207","has_issues":true}
     ];
     console.log(this.user);
-    console.log(this.repositories);
+    console.log(this.repositories);*/
 
-    /*this.githubservice.getUser().subscribe(
-      user=>{
-        this.user=user;
-        console.log(this.user);
-        console.log(this.user.avatar_url);
-    });
+    this.githubservice.getUser().subscribe(
+      x => this.user=x,
+      err => console.error('Observer got an error: ' + err),
+      () => console.log('Observer completed')
+    );
 
     this.githubservice.getRepos().subscribe(
-      repositories=>{
-      this.repositories = repositories;
-      console.log(this.repositories);
-    });*/
+      x => this.repositories = x,
+      err => console.error('Observer got an error: ' + err),
+      () => console.log('Observer completed')
+    );
   }
   ngOnInit() {
   }
